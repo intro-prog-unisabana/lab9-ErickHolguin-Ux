f4a7d9da42491d7e0cbabb5a2740f04767b06e0d
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-05-01 12:49:43 -0500

    Problema 4

--- car.py
+++ car.py
@@ -1,2 +1,17 @@
 # Write your class definition here!
+class Car:
+    def __init__(self, car_id, brand, year, color, mileage):
+        self.car_id = car_id
+        self.brand = brand
+        self.year = year
+        self.color = color
+        self.mileage = mileage
 
+    def drive(self, miles):
+        self.mileage += miles
+
+    def change_color(self, new_color):
+        self.color = new_color
+
+    def __str__(self):
+        return f"{self.car_id} - {self.year} {self.color} {self.brand} with {self.mileage} miles"
