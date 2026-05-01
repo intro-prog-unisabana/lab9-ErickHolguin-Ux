058e33c23bdc7c9426fd3a7f4517aa7da5c0ec40
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-05-01 14:27:41 -0500

    pro

---
+++ aircraft_altitude.py
@@ -0,0 +1,17 @@
+from aircraft import Aircraft
+
+model = input("Enter aircraft model:\n")
+aircraft = Aircraft(model)
+
+while True:
+    command = input("Enter command (A for ascent, D for descent, X to exit):\n")
+    parts = command.split()
+
+    if parts[0] == "A":
+        aircraft.climb(int(parts[1]))
+    elif parts[0] == "D":
+        aircraft.descend(int(parts[1]))
+    elif parts[0] == "X":
+        break
+
+print(f"Final altitude: {aircraft.altitude} feet")
